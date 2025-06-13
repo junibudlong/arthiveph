1a7ad08a6050014a93520dd181c88da7e00c3d00
junibudlong <jbudlong17@gmail.com>
2025-06-13 17:54:35 +0800

    Update page.tsx

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -2,9 +2,18 @@
 import { supabase } from '@/lib/supabase';
 import ProductDisplay from '@/components/ProductDisplay';
 import { notFound } from 'next/navigation';
-import type { PageProps } from '@/app/products/[productId]/page.d';
 
-export default async function ProductPage({ params }: PageProps) {
+
+interface ProductPageParams {
+  productId: string;
+  title: string;
+  price: number;
+  image_url: string;
+  description?: string;
+  artist_id: string;
+}
+
+export default async function ProductPage(params: ProductPageParams) {
   const { productId } = params;
 
   const { data: product, error } = await supabase
